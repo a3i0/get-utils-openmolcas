7505ec1597d43fda77432d3cd7c777dff859b7e3
Imaad <imaad@pop-os.localdomain>
2020-02-06 18:39:19 +0100

    Directory execution update!
    Now the scripts can be in any directory and executed via bash <path/to/getmoments.sh> file.log.
    Requires getenergies.sh, getvalues.sh and getmoments.py to be in the
    same directory as getmoments.sh

--- getenergies.sh
+++ getenergies.sh
@@ -1,1 +1,2 @@
-grep "RASSI State" mol2.log | cut -c 1-43 --complement | cat > energies.raw
+#v 1.0.1
+grep "RASSI State" $1 | cut -c 1-43 --complement | cat > energies.raw
--- getmoments.sh
+++ getmoments.sh
@@ -1,6 +1,8 @@
-#v1.2
+#v1.2.1
 #Input: getvalues.sh, .log file
 #Output: readeable files (with dipole, angular momentum and velocity componenets), raw files for python (.raw)
+
+SCRIPTS_DIR=$(dirname $0) #variable with the directory of all the script files
 
 #creating readable data file with dipole, angular momentum and velocity (momentum) components
 grep --after-context=8 "PROPERTY: MLTPL  1   COMPONENT:   1" $1 | sed '2,3 d' | sed '3 d' | cat > moments.dat
@@ -19,10 +21,10 @@ grep --after-context=8 "PROPERTY: VELOCITY   COMPONENT:   3" $1 | sed '2,3 d' | 
 
 #creating raw data file to be loaded on to python
 cat moments.dat | sed '/PROPERTY/ d' | sed '/STATE/ d' | cat > moments.raw
-bash getvalues.sh $1 | sed '17 !d' | cat > momentsfullop.raw
+bash $SCRIPTS_DIR/getvalues.sh $1 | sed '17 !d' | cat > momentsfullop.raw
 #grep --after-context=6 "Dipole transition strengths (spin-free states)" mol2.log | tail -1 | cat > ostr_raw.dat
 #grep --after-context=6 "Velocity transition strengths (spin-free states):" mol2.log | tail -1 | cat >> ostr_raw.dat
-bash getenergies.sh #creates energies.raw
+bash $SCRIPTS_DIR/getenergies.sh $1 #creates energies.raw
 
-python getmoments.py #calculates u.m and tiintensity rot strength in cgs units
+python $SCRIPTS_DIR/getmoments.py #calculates u.m and tiintensity rot strength in cgs units
 
