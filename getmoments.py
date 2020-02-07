Corrected incorrect units
Dipole strength with correct conversion from a.u to cgs (https://en.wikipedia.org/wiki/Hartree_atomic_units
and https://www.plasmaphysics.org.uk/convers.htm)
Oscillator strength now corrected to be unitless
Relevant definitons of variables removed

getmoments.py
@@ -1,4 +1,4 @@
-#v1.3
+#v1.3.1
 #Input: RASSI State energies, Dipole, velocity and angular momentum components, full operator oscillator and rotatory strengths
 #Output: Rot strength in mixed, veocity and for full operator, dipole strength in length, osc strength in velociy and full operator
 #Dependencies: look at files loaded by np.loadtxt(). Should be in correct format
@@ -27,34 +27,30 @@ rot_str_vel=-1*np.dot(v,m)/E1
 rot_str_vel_cgs=rot_str_vel*471.44e-40
 #dipole strengths in length and velocity 
 dip_str_len=abs(np.dot(u,u))
-dip_str_len_cgs=dip_str_len*471.44e-40
+dip_str_len_cgs=dip_str_len*64591e-40
 dip_str_vel=abs(np.dot(v,v))
 #oscillatory strength in velocity
 osc_str_vel=2.0*dip_str_vel/(3.0*E1)
-osc_str_vel_cgs=osc_str_vel*471.44e-40
 
 #full operator moments
 fullopdata=np.loadtxt("momentsfullop.raw") 
 rot_str_fullop=fullopdata[3]*1.967e-3 #converting from reduced rot str to atomic units
 rot_str_fullop_cgs=rot_str_fullop*471.44e-40 #converting from a.u to cgs
-osc_str_fullop=fullopdata[2] #already in atomic units
-osc_str_fullop_cgs=osc_str_fullop*471.44e-40
+osc_str_fullop=fullopdata[2] #unitless
 
 #osdata=np.loadtxt("ostr_raw.dat")
 #osc_strength_len=osdata[0,2]
 #osc_strength_vel=osdata[1,2]
-#osc_strength_len_cgs=osc_strength_len*471.44e-40
-#osc_strength_vel_cgs=osc_strength_vel*471.44e-40
 
 file=open("moments.dat","a")
 file.writelines(["Rotatory strengths (mixed): ", str(rot_str), "(a.u) ", str(rot_str_cgs), "(cgs) ", "\n"])
 file.writelines(["Rotatory strengths (velocity): ", str(rot_str_vel), "(a.u) ", str(rot_str_vel_cgs), "(cgs) ", "\n"])
 file.writelines(["Rotatory strengths (full operator):", str(rot_str_fullop), "(a.u) " , str(rot_str_fullop_cgs), "(cgs)", "\n"])
 file.writelines(["Dipole strengths (length): " , str(dip_str_len), "(a.u) ",  str(dip_str_len_cgs), "(cgs)", "\n"])
-file.writelines(["Oscillator strengths (velocity): " , str(osc_str_vel), "(a.u) ",  str(osc_str_vel_cgs), "(cgs)", "\n"])
-file.writelines(["Oscillator strengths (full operator):", str(osc_str_fullop), "(a.u) " , str(osc_str_fullop_cgs), "(cgs)", "\n"])
+file.writelines(["Oscillator strengths (velocity): " , str(osc_str_vel), "\n"])
+file.writelines(["Oscillator strengths (full operator):", str(osc_str_fullop), "\n"])
 
-#file.writelines(["Osccilator strengths (velocity): " , str(osc_strength_vel), "(a.u) ",  str(osc_strength_vel_cgs), "(cgs)", "\n"])
+#file.writelines(["Osccilator strengths (velocity): " , str(osc_strength_vel), "\n"])
 
 file.close()
 