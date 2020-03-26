4368494eba6dd997c9ec62ded1d7847231f04495
Imaad <imaad@pop-os.localdomain>
2020-03-26 16:53:20 +0100

    Now does not print number of directions on top of each file.

--- directional/getdirections.py
+++ directional/getdirections.py
@@ -1,42 +1,46 @@
 #v1.0
 #Input = geom file, grid size(factor by which to divide 360). 
-#Output = directions.inp, a list of directions along xy and xz concatenated. Has number of directions on top of file. 
+#Output = directions.inp, a list of directions in cartesian and spherical coordinates. 
 #syntax: python3 getdirections.py <input_geom> <grid size>
 
 import numpy as np
 import sys
 
-geom=np.loadtxt(sys.argv[1],skiprows=2, usecols=(1,2,3)) #skips first 2 rwos and reads column 2,3 and 4 from first argument passed to py file
+geom=np.loadtxt(sys.argv[1],skiprows=2, usecols=(1,2,3)) #skips first 2 rows and reads column 2,3 and 4 from first argument passed to py file
 natoms=np.loadtxt(sys.argv[1],max_rows=1) #reads first row of input file
 
 com=sum(geom)/natoms #centre of mass. sum() sums up rows 
 angle_grid_size=int(sys.argv[2]) #divides 360 degrees into angle_grid_size parts in both xy and xz planes
 
-#Rotation matrices
-givens_xy=givens_xy=np.array(([np.cos(2*np.pi/angle_grid_size) , -np.sin(2*np.pi/angle_grid_size) , 0], [np.sin(2*np.pi/angle_grid_size) , np.cos(2*np.pi/angle_grid_size) , 0], [0,0,1]))
-givens_xz=np.array(([np.cos(2*np.pi/angle_grid_size) , 0, -np.sin(2*np.pi/angle_grid_size)], [0,1,0], [np.sin(2*np.pi/angle_grid_size) , 0, np.cos(2*np.pi/angle_grid_size)]))
-
-#Initialising direction matrices as natoms x 3 matrices
-directions_xy=np.zeros(shape=((angle_grid_size),3))
-directions_xz=np.zeros(shape=((angle_grid_size),3))
-directions=np.zeros(shape=((angle_grid_size*angle_grid_size),3))
-
-#updating entries
-for i in range(angle_grid_size):
-    if (i==0):
-        directions_xy[i]=np.array([1.0,0.0,0.0])
-        continue
-    directions_xy[i]=np.matmul(givens_xy,directions_xy[i-1])
+theta=np.linspace(0,360,angle_grid_size+1)
+phi=np.linspace(-90,90,angle_grid_size+1)
+r=1
+directions_sph=np.zeros(shape=((angle_grid_size+1)**2,3))
 
 index=0
-for i in range(angle_grid_size):
-    for j in range(angle_grid_size):
+for i in range(angle_grid_size+1):
+    for j in range(angle_grid_size+1):
         index=index+1
-        if(j==0):
-           directions[index-1]=directions_xy[i]
-           continue
-        else:
-            directions[index-1]=np.matmul(givens_xz,directions[index-2])
+        directions_sph[index-1,0]=theta[i]
+        directions_sph[index-1,1]=phi[j]
+        directions_sph[index-1,2]=1
+
+x=np.cos(theta*(np.pi/180.0))*np.cos(phi*(np.pi/180.0))
+y=np.sin(theta*(np.pi/180.0))*np.cos(phi*(np.pi/180.0))
+z=np.sin(phi*(np.pi/180.0))
+directions_cart=np.zeros(shape=((angle_grid_size+1)**2,3))
+
+index=0
+for i in range(angle_grid_size+1):
+    for j in range(angle_grid_size+1):
+        index=index+1
+        directions_cart[index-1,0]=np.cos(directions_sph[index-1,0]*(np.pi/180.0))*np.cos(directions_sph[index-1,1]*(np.pi/180.0))
+        directions_cart[index-1,1]=np.sin(directions_sph[index-1,0]*(np.pi/180.0))*np.cos(directions_sph[index-1,1]*(np.pi/180.0))
+        directions_cart[index-1,2]=np.sin(directions_sph[index-1,1]*(np.pi/180.0))
+
+
+
+
                 
 #shifting by adding com
 #directions_xy=com+directions_xy
@@ -46,4 +50,6 @@ for i in range(angle_grid_size):
 #alldir=np.concatenate((directions_xy,directions_xz),axis=0) #concatenates matrices along the column direction
 
 #saving in appropriate format for inputting into molcas. header adds lines before data and comments defaults to '#')
-np.savetxt('directions.inp', directions, header=str(len(directions_xy)*len(directions_xz)), comments='')
+np.savetxt('directions_sph.inp', directions_sph)
+np.savetxt('directions_cart.inp', directions_cart)
+
