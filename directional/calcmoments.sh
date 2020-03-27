59da20f220eb73b26a4196915e19fd1c962386e4
Imaad <imaad@pop-os.localdomain>
2020-03-27 14:34:29 +0100

    Now also extracts and calculates truncate operator directional moments

--- directional/calcmoments.sh
+++ directional/calcmoments.sh
@@ -1,24 +1,24 @@
 #v1.0
-#Input: .log file, number of roots, roots between which transition moments are to be calculated
+#Input: .log file, number of roots, roots between which transition moments are to be calculated. getfullopmoments,sh, getmoments.sh, getmoments.py
 #Output: raw files with directional (one file for each) full operator transition moments for python (.raw). Also calls python to do calculations.
 #syntax: bash calcmoments.sh <.log> <nroots> <root1> <root2>
 
 SCRIPTS_DIR=$(dirname $0) #variable with the directory of all the script files
-
+nroots=$2
 
 #array with all the lines between which data is located
 dataholders=($(bash $SCRIPTS_DIR/getfullopmoments.sh $1 $2 | grep -n '\---------------------------------------------------------------'| cut -d ":" -f 1))
 len_dataholders=${#dataholders[@]} #length of this array
 
 rm -f momentsfullop* #removing file to prevent append errors in python
-rm -f tmoments.dat 
+rm -f tmoments.dat
 
 #File with all the directions
 #bash $SCRIPTS_DIR/getdirections.sh $1 $2 > directions.raw
- 
+
 #Creating one data file for each direction
 i=0
-while ((i<$((len_dataholders/2)))) 
+while ((i<$((len_dataholders/2))))
 do
         startline=${dataholders[$((2*i))]}
         endline=${dataholders[$(((2*i)+1))]}
@@ -28,6 +28,33 @@ do
         let i++
 done
 
+#Truncated Operators
+#Creating velmoments.dat and mixmoment.dat
+bash $SCRIPTS_DIR/getmoments.sh $1
+
+ndir=$((len_dataholders/2))
+i=0
+#rm -f velmoments.raw
+
+sed '1,4 d' velmoments.dat | sed '/Direction/d' | sed '/From/d' | sed '/--/d' | sed "s/^ *//g" > velmoments.raw
+sed '1,9 d' mixmoments.dat | sed '/Direction/d' | sed '/From/d' | sed '/--/d' | sed "s/^ *//g" > mixmoments.raw
+#while ((i<$ndir))
+#do
+#  grep 'Red. rot. str.' velmoments.dat -m $((i+1)) --after-context=$(((nroots*(nroots-1))/2 + 1)) | sed '1,2 d' >> velmoments.raw
+#  let i++
+#done
+#i=0
+#rm -f mixmoments.raw
+#while ((i<$ndir))
+#do
+#  grep 'Red. rot. str.' mixmoments.dat -m $((i+1)) --after-context=$(((nroots*(nroots-1))/2 + 1)) | sed '1,2 d' >> mixmoments.raw
+#  let i++
+#done
+
+
+
 python3 $SCRIPTS_DIR/getmoments.py $2 $3 $4 $((len_dataholders/2))
 #removing temporary files created by the data getters above
 rm momentsfullop*
+#rm velmoments.raw
+#rm mixmoments.raw
--- directional/getmoments.py
+++ directional/getmoments.py
@@ -1,7 +1,8 @@
 #v1.1
 #Input: full operator oscillator and rotatory strengths for each direction, number of roots, intial and final roots and number of directions.
-#Direction file directions_cart.inp
-#Output: Directional rot strentght and oscillator strength for full operator. All for specified roots
+#Direction file directions_cart.inp, files loaded by np.loadtxt()
+#Output: Directional rot strentght and oscillator strength for full operator.
+#        Directional Rotatory strengths for mixed and veloxity gauge. All for specified roots
 #Syntax: python3 getmoments.sh <nroots> <initial root> <final root> <number of directions>
 #Dependencies: look at files loaded by np.loadtxt(). Should be in correct format
 
@@ -20,7 +21,7 @@ ndir=1
 if(sys.argv[1] != 0):
     nroots=int(sys.argv[1])
 
-if(sys.argv[2]!=0 and sys.argv[3]!=0): 
+if(sys.argv[2]!=0 and sys.argv[3]!=0):
     ri=int(sys.argv[2])
     rf=int(sys.argv[3])
 
@@ -47,11 +48,11 @@ while (i<=ndir):
     filename_3=".raw"
     filename=filename_1+filename_2+filename_3
     fullopdata=np.loadtxt(filename)
-    
+
     #default transition matrix values are zero
     fulloprot = np.zeros(shape=(nroots,nroots))
     fulloposc = np.zeros(shape=(nroots,nroots))
-    
+
 
 
     #checking the indices of transition and filling the transition matrix correspondingly
@@ -62,11 +63,11 @@ while (i<=ndir):
         fulloprot[final-1,init-1]=fulloprot[init-1,final-1] #symmetrising
         fulloposc[init-1,final-1]=fullopdata[j,2]
         fulloposc[final-1,init-1]=fulloposc[init-1,final-1] #symmetrising
-    
+
     rot_str_fullop=fulloprot[ri-1,rf-1]*1.967e-3 #converting from reduced rot str to atomic units
     rot_str_fullop_cgs=rot_str_fullop*471.44e-40 #converting from a.u to cgs
     osc_str_fullop=fulloposc[ri-1,rf-1] #unitless
-    
+
     file=open("dir-fullmoments.raw","a")
     #file.writelines(["Rotatory strengths (mixed): ", str(rot_str), " (a.u) ", str(rot_str_cgs), " (cgs) ", "\n"])
     #file.writelines(["Rotatory strengths (velocity): ", str(rot_str_vel), " (a.u) ", str(rot_str_vel_cgs), " (cgs) ", "\n"])
@@ -76,10 +77,42 @@ while (i<=ndir):
     #file.writelines(["Excitation Energy: ", str(E1), " (a.u) ", str(E1_ev), " (eV) ", "\n"])
     #file.writelines(["Osccilator strengths (velocity): " , str(osc_strength_vel), "\n"])
     file.close()
-    
+
     i=i+1
     continue
 
+#Truncated Operator moments
+velmoments=np.zeros(shape=(nroots,nroots,ndir)) #3D array with all moments of all directions
+veldata=np.loadtxt("velmoments.raw")
+for k in range(ndir): #This loop assumes that velmoments.raw and mixmoments.raw have moments for all roots
+    for i in range(nroots):
+        for j in range(i+1,nroots):
+            velmoments[i,j,k] = veldata[j-(i+1) + i*nroots - i*(i+1)//2 + k*(nroots*(nroots-1))//2 , 2]
+
+mixmoments=np.zeros(shape=(nroots,nroots,ndir)) #3D array with all moments of all directions
+mixdata=np.loadtxt("mixmoments.raw")
+for k in range(ndir): #This loop assumes that velmoments.raw and mixmoments.raw have moments for all roots
+    for i in range(nroots):
+        for j in range(i+1,nroots):
+            mixmoments[i,j,k] = mixdata[j-(i+1) + i*nroots - i*(i+1)//2 + k*(nroots*(nroots-1))//2 , 2]
+
+#Converting to cgs
+velmoments_cgs=velmoments*9.274E-41
+mixmoments_cgs=mixmoments*9.274E-41
+
+#printing to file
+if os.path.exists("dir-velmoments.raw"):
+        os.remove("dir-velmoments.raw")
+if os.path.exists("dir-mixmoments.raw"):
+        os.remove("dir-mixmoments.raw")
+
+for i in range(ndir):
+    file1=open("dir-velmoments.raw","a")
+    file2=open("dir-mixmoments.raw","a")
+
+    file1.writelines([str(directions[i,0])," ",str(directions[i,1])," ",str(directions[i,2]), " ", str(velmoments[ri-1,rf-1,i]), " ", str(velmoments_cgs[ri-1,rf-1,i]), "\n"])
+    file2.writelines([str(directions[i,0])," ",str(directions[i,1])," ",str(directions[i,2]), " ", str(mixmoments[ri-1,rf-1,i]), " ", str(mixmoments_cgs[ri-1,rf-1,i]), "\n"])
 
 
-
+    file1.close()
+    file2.close()
