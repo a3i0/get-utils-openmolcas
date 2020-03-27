df1b3c6ea70fcaac6a91417eda3cb19c04adf796
Imaad <imaad@pop-os.localdomain>
2020-03-27 14:35:26 +0100

    Removed unneccesary variables not used in script

--- directional/getmoments.sh
+++ directional/getmoments.sh
@@ -1,9 +1,7 @@
 #Input:
 #Output: Files with directional velocity gague and directional mixed gauge CD values
 
-nroots=$2
-ri=$2 #Initial and final roots
-rf=$3
+
 
 line_init_vel=$(grep 'Circular Dichroism' $1  --line-number | cut -d ":" -f 1 | sed '2,3 d')
 line_final_vel=$(grep 'Circular Dichroism' $1  --line-number | cut -d ":" -f 1 | sed '1 d'| sed '2 d')
