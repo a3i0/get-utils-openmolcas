a215f5272bbf74c95d2fba379683ece6fb093ae7
Imaad <imaad@pop-os.localdomain>
2020-05-13 21:24:46 +0200

    Separate length gauge dipole, velocity gauge dipole and magnetic dipole vectors

--- molecule_property-images/testscript.py
+++ molecule_property-images/testscript.py
@@ -3,49 +3,47 @@ from ovito.vis import VectorVis
 from ovito.vis import ParticlesVis
 import numpy as np
 
-vector_vis = VectorVis(
-    alignment = VectorVis.Alignment.Base, 
+dip_len_vis = VectorVis( #vector visual element that needs to be attached to the relevant property
+    alignment = VectorVis.Alignment.Base,
     color = (1.0, 0.0, 0.4),
     width = 0.05 )
 
-#transparency_vis = ParticlesVis(
-#    transparency=0.1 )
+dip_vel_vis = VectorVis( #vector visual element that needs to be attached to the relevant property
+    alignment = VectorVis.Alignment.Base,
+    color = (0.0, 0.0, 0.4),
+    width = 0.05 )
 
+mag_vis = VectorVis( #vector visual element that needs to be attached to the relevant property
+    alignment = VectorVis.Alignment.Base,
+    color = (1.0, 0.0, 0.0),
+    width = 0.05 )
     
 def modify(frame, data):
-    
-    # This user-defined modifier function gets automatically called by OVITO whenever the data pipeline is newly computed.
-    # It receives two arguments from the pipeline system:
-    # 
-    #    frame - The current animation frame number at which the pipeline is being evaluated.
-    #    data   - The DataCollection passed in from the pipeline system. 
-    #                The function may modify the data stored in this DataCollection as needed.
-    # 
-    # What follows is an example code snippet doing nothing except printing the current 
-    # list of particle properties to the log window. Use it as a starting point for developing 
-    # your own data modification or analysis functions. 
-    
-    
-            
-    
-    
+
     pos=np.array(data.particles['Position'])
-    com=sum(pos)/data.particles.count
-    com_repeated=np.tensordot(np.ones(data.particles.count),com,axes=0)
+    com=sum(pos)/data.particles.count #centre of mass
+    com_repeated=np.tensordot(np.ones(data.particles.count),com,axes=0) #formatting so that the array passed to create_property is of correct dimensions '(Nx3)'
     prop_com = data.particles_.create_property('Centre of Mass', data=com_repeated, components=3)
-    
-    com_as_particle_index=data.particles_.create_particle(com)
-    print(com)
-    transparency=np.zeros(data.particles.count)
-    transparency[com_as_particle_index]=1
-    prop_transparency = data.particles_.create_property('Transparency', data=transparency)
-    
-    testvector=np.zeros(shape=(data.particles.count,3))
-    testvector[com_as_particle_index]=[1,1,1]
-    my_prop = data.particles_.create_property('My Property', data=testvector,components=3)
-    my_prop.vis = vector_vis
-    
-    if data.particles != None:
-        print("There are %i particles with the following properties:" % data.particles.count)
-        for property_name in data.particles.keys():
-            print("  '%s'" % property_name)
+
+    com_as_particle_index = data.particles_.create_particle(com) #adds com as a particle and returns its index
+    transparency = np.zeros(data.particles.count)
+    transparency[com_as_particle_index]=1 #com particle fully transparent
+    data.particles_.create_property('Transparency', data=transparency)
+
+    #Adding Dipole in Length gauge
+    dip_len=np.zeros(shape=(data.particles.count,3))
+    dip_len[com_as_particle_index]=[1,0,0] #value of the dipole that will be rendered only at the com particle
+    dip_len_prop = data.particles_.create_property('Dipole Length Gauge', data=dip_len, components=3)
+    dip_len_prop.vis = dip_len_vis
+
+    #Adding Dipole in Velocity gauge
+    dip_vel=np.zeros(shape=(data.particles.count,3))
+    dip_vel[com_as_particle_index]=[0,1,0] #value of the dipole that will be rendered only at the com particle
+    dip_vel_prop = data.particles_.create_property('Dipole Velocity Gauge', data=dip_vel, components=3)
+    dip_vel_prop.vis = dip_vel_vis
+
+    #Adding magnetic dipole
+    mag=np.zeros(shape=(data.particles.count,3))
+    mag[com_as_particle_index]=[0,0,1] #value of the dipole that will be rendered only at the com particle
+    mag_prop = data.particles_.create_property('Magnetic Dipole', data=mag, components=3)
+    mag_prop.vis = mag_vis
