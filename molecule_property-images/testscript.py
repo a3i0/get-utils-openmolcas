576b8ebb3cfb14b57cbc3c82c8e1a8421ea61d85
Imaad <imaad@pop-os.localdomain>
2020-05-13 21:30:38 +0200

    Changed colours Chamged length of vectors to 3

--- molecule_property-images/testscript.py
+++ molecule_property-images/testscript.py
@@ -5,19 +5,19 @@ import numpy as np
 
 dip_len_vis = VectorVis( #vector visual element that needs to be attached to the relevant property
     alignment = VectorVis.Alignment.Base,
-    color = (1.0, 0.0, 0.4),
+    color = (1.0, 0.0, 0.0),
     width = 0.05 )
 
 dip_vel_vis = VectorVis( #vector visual element that needs to be attached to the relevant property
     alignment = VectorVis.Alignment.Base,
-    color = (0.0, 0.0, 0.4),
+    color = (0.9, 0.4, 0.0),
     width = 0.05 )
 
 mag_vis = VectorVis( #vector visual element that needs to be attached to the relevant property
     alignment = VectorVis.Alignment.Base,
-    color = (1.0, 0.0, 0.0),
+    color = (0.0, 0.0, 1.0),
     width = 0.05 )
-    
+
 def modify(frame, data):
 
     pos=np.array(data.particles['Position'])
@@ -32,18 +32,18 @@ def modify(frame, data):
 
     #Adding Dipole in Length gauge
     dip_len=np.zeros(shape=(data.particles.count,3))
-    dip_len[com_as_particle_index]=[1,0,0] #value of the dipole that will be rendered only at the com particle
+    dip_len[com_as_particle_index]=[3,0,0] #value of the dipole that will be rendered only at the com particle
     dip_len_prop = data.particles_.create_property('Dipole Length Gauge', data=dip_len, components=3)
     dip_len_prop.vis = dip_len_vis
 
     #Adding Dipole in Velocity gauge
     dip_vel=np.zeros(shape=(data.particles.count,3))
-    dip_vel[com_as_particle_index]=[0,1,0] #value of the dipole that will be rendered only at the com particle
+    dip_vel[com_as_particle_index]=[0,3,0] #value of the dipole that will be rendered only at the com particle
     dip_vel_prop = data.particles_.create_property('Dipole Velocity Gauge', data=dip_vel, components=3)
     dip_vel_prop.vis = dip_vel_vis
 
     #Adding magnetic dipole
     mag=np.zeros(shape=(data.particles.count,3))
-    mag[com_as_particle_index]=[0,0,1] #value of the dipole that will be rendered only at the com particle
+    mag[com_as_particle_index]=[0,0,3] #value of the dipole that will be rendered only at the com particle
     mag_prop = data.particles_.create_property('Magnetic Dipole', data=mag, components=3)
     mag_prop.vis = mag_vis
