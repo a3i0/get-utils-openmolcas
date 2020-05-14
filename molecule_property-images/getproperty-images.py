1d6520534dd55d5bd8ad17fb50000be17322eae7
Imaad <imaad@pop-os.localdomain>
2020-05-14 16:30:30 +0200

    Fixed file format transformation bug Now renders with chain-of-command using getproperty-images.sh but without the vectors

--- molecule_property-images/getproperty-images.py
+++ molecule_property-images/getproperty-images.py
@@ -1,5 +1,5 @@
 #Input: dip_len_value.raw, dip_vel_value.raw, mag_value.raw, xyz_file.xyz coordinates of particles
-#Output: xyz_file.png rendered with vectors specified by .raw input files.
+#Output: xyz_file.png rendered with vectors specified by .raw input files. Vectors scaled to have norm 2.
 #Requirements: Python module ovito and ovito installed on system.
 #              OpenGL rendered requires graphics card, so may not work on remote servers.
 #Syntax: python3 getproperty-images.py xyz_file.xyz
@@ -18,7 +18,7 @@ dip_len_value=np.loadtxt('dip_len_value.raw')
 dip_vel_value=np.loadtxt('dip_vel_value.raw')
 mag_value=np.loadtxt('mag_value.raw')
 
-#scaling all values so that their norms are sqrt(2)
+#scaling all values so that their norms are 2.
 dip_len_value= (np.sqrt(2)/np.linalg.norm(dip_len_value))*dip_len_value
 dip_vel_value= (np.sqrt(2)/np.linalg.norm(dip_vel_value))*dip_vel_value
 mag_value= (np.sqrt(2)/np.linalg.norm(mag_value))*mag_value
@@ -38,7 +38,7 @@ pipeline.modifiers.append(CreateDoublebonds())
 pipeline.modifiers.append(CreateCCbonds())
 pipeline.modifiers.append(CreateCHbonds())
 pipeline.modifiers.append(good_ballandstick)
-pipeline.modifiers.append(VisualiseMoments)
+#pipeline.modifiers.append(VisualiseMoments)
 
 #add pipeline to visual scene
 pipeline.add_to_scene()
@@ -57,7 +57,7 @@ vp.zoom_all() #note that this resets camera_pos but not camera_dir
 osp=OSPRayRenderer()
 osp.max_ray_recursion=4
 
-png_file=xyz_file+str.replace('.xyz','.png')
+png_file=xyz_file.replace(".xyz" , ".png" )
 #vp.render_image(filename='output.png',size=(1280,720), alpha=True, renderer=OpenGLRenderer())
-vp.render_image(filename='output.png',size=(1280,720), renderer=OpenGLRenderer()) #opengl without transparency
+vp.render_image(filename=png_file,size=(1280,720), renderer=OpenGLRenderer()) #opengl without transparency
 #vp.render_image(filename='output.png',size=(1280,720), alpha=true, renderer=osp) #Raytraced OSPRayRenderer
