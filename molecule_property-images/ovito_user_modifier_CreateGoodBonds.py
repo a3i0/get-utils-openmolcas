2216da3d474ac6718c9899775c48e0e15a3f1112
Imaad <imaad@pop-os.localdomain>
2020-05-14 14:31:04 +0200

    Now shifted all bond making modifiers to ovito_user_modifier_CreateGoodBonds.py. It has both actual modifiers and functions that return modifiers.

--- molecule_property-images/ovito_user_modifier_CreateGoodBonds.py
+++ molecule_property-images/ovito_user_modifier_CreateGoodBonds.py
@@ -15,26 +15,35 @@ Bond2 = BondType(
     color=(0.3,0.3,0.4)
 )
 
-def CreateBonds(frame,data):
+def CreateCHbonds():
     create_CHbonds=CreateBondsModifier(mode=CreateBondsModifier.Mode.Pairwise)
     create_CHbonds.set_pairwise_cutoff('C','H',1.2)
     create_CHbonds.bond_type=Bond1
     create_CHbonds.vis.use_particle_colors=False
+    # create_CHbonds.vis.width=0.1
 
+    return create_CHbonds
+
+def CreateCCbonds():
     create_CCbonds=CreateBondsModifier(mode=CreateBondsModifier.Mode.Pairwise, lower_cutoff=1.4)
     create_CCbonds.set_pairwise_cutoff('C','C',1.6)
     create_CCbonds.bond_type=Bond1
     create_CCbonds.vis.use_particle_colors=False
+    # create_CCbonds.vis.width=0.1
 
+    return create_CCbonds
+
+def CreateDoublebonds():
     create_doublebonds=CreateBondsModifier(mode=CreateBondsModifier.Mode.Pairwise)
     create_doublebonds.set_pairwise_cutoff('C','O',1.3)
     create_doublebonds.set_pairwise_cutoff('C','C',1.4)
     create_doublebonds.bond_type=Bond2
     create_doublebonds.vis.use_particle_colors=False
     #create_bonds.set_pairwise_cutoff('C','C',1.6)
-
-    # create_CHbonds.vis.width=0.1
     # create_doublebonds.vis.width=0.3
+
+    return create_doublebonds
+
 
 def good_ballandstick(frame,data):
     particle_type=data.particles['Particle Type']
@@ -54,3 +63,9 @@ def good_ballandstick(frame,data):
     #data.particles.bonds.vis.shading = BondsVis.Shading.Flat
     data.particles.bonds.vis.width = 0.2
     # print(data.particles_.bonds_.bond_types_.type_by_id(2).color)
+
+    # types = pipeline.source.data.particles_.particle_types_
+    # data.particle_types.make_mutable(types)
+    # types.type_by_name('C').radius = 0.3
+    # types.type_by_name('H').radius = 0.1
+    # types.type_by_name('O').radius = 0.4
